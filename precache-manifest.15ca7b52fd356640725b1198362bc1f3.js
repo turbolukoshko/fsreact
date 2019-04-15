82031d55002df59521d07c178a1d5bf704e8c02e
Vladyslav <turbo.lukoshko@gmail.com>
2019-04-15 23:49:24 +0300

    build

---
+++ precache-manifest.15ca7b52fd356640725b1198362bc1f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b5cdd7306e5588787163",
+    "url": "/static/js/main.040d35aa.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b5cdd7306e5588787163",
+    "url": "/static/css/main.2ae8bb2e.chunk.css"
+  },
+  {
+    "revision": "79d3c3bd155737151f2a59ec29f91847",
+    "url": "/index.html"
+  }
+];
